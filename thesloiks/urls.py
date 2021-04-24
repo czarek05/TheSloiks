9eedded1729122e35da2ac52e7790860978d40e3
Czarek D <czaro05@wp.pl>
2021-04-24 03:06:01 +0200

    Remove unneeded comments

--- thesloiks/urls.py
+++ thesloiks/urls.py
@@ -8,17 +8,3 @@ urlpatterns = [
     path('jar/<int:jar_id>', views.jar, name='jar_with_id'),
     path('transaction/', views.transaction, name='transaction'),
 ]
-
-
-'''
-API
-GET /thesloiks/jar wyswietla wszystkie sloiki (id, balance, currency)
-GET /thesloiks/jar/1 wyswietla sloik o id 1
-POST /thesloiks/jar JSON (stworzenie sloika)
-
-POST /thesloiks/transaction JSON (włożenie, wyjęcie, transfer środków)
-GET /thesloiks/transaction?jarId=1 przejrzenie historii operacji w sloiku
-GET /thesloiks/transaction?jarId=1&sortBy=balance&sortOrder=desc przejrzenie posortowanej historii operacji w sloiku
-GET /thesloiks/transaction przejrzenie historii wszystkich operacji
-GET /thesloiks/transaction?jarId=1,2 przejrzenie historii wszystkich operacji filtrowanej
-'''
